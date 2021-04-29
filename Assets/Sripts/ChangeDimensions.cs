c76db7657d421f551d6d9c02fc2778ed3756901e
Hit-Boy <sunguy2@mail.ru>
2021-04-29 14:59:32 +0300

    Working ChangeDimensions

---
+++ Assets/Sripts/ChangeDimensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ChangeDimensions : MonoBehaviour
+{
+
+    public decimal stateOfDimension = 1m;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            switch (stateOfDimension)
+            {
+                case 1:
+                    stateOfDimension = 2m;
+                    break;
+                case 2:
+                    stateOfDimension = 1m;
+                    break;
+                default:
+                    Debug.Log("ChangeDimensions broke");
+                    break;
+
+            }
+        }
+
+    }
+}
---
+++ Assets/Sripts/TraverseDimensions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TraverseDimensions : MonoBehaviour
+{
+
+    ChangeDimensions changeDimensionsScript;
+    decimal currentWorld = 1m;
+
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+        changeDimensionsScript = player.GetComponent<ChangeDimensions>();
+    }
+
+    private void Update()
+    {
+        if (changeDimensionsScript.stateOfDimension != currentWorld)
+        {
+            Debug.Log("Checked " + changeDimensionsScript.stateOfDimension + " " + currentWorld);
+            ChangeWorlds();
+            currentWorld = changeDimensionsScript.stateOfDimension;
+        }
+    }
+    private void ChangeWorlds()
+    {   
+        Renderer dimensionObjectRenderer = GetComponent<Renderer>();
+        Collider dimensionObjectCollider = GetComponent<Collider>();
+      //  Renderer shadowObjectRenderer = GetComponentInChildren<Renderer>();
+
+        dimensionObjectCollider.enabled = !dimensionObjectCollider.enabled;
+        dimensionObjectRenderer.enabled = !dimensionObjectRenderer.enabled;
+       // shadowObjectRenderer.enabled = !shadowObjectRenderer.enabled;
+    }
+}
